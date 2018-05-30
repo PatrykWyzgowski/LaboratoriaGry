d4c38eb0c7a2e098531b9e019993fb210c74e23c
Arek Szwarc <arek078@gmail.com>
2018-05-30 11:32:09 +0200

    Edycja ruch gracza

--- Game - River Raid/Assets/Scripts/RuchGracza.cs
+++ Game - River Raid/Assets/Scripts/RuchGracza.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
 public class RuchGracza : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public float speed;
+    public float tilt;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector3
+        (
+            Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+            0,
+            Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+        );
+
+        rb.rotation = Quaternion.Euler(0, 0, rb.velocity.x * -tilt);
+    }
 }
