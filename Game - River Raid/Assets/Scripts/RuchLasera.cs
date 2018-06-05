a1a67b5d731efa4a0bef03c932adc97f9bedfd4e
pastrelic <patrykwyzgowskig9@wp.pl>
2018-06-05 21:09:18 +0200

    Dodano strzelanie

---
+++ Game - River Raid/Assets/Scripts/RuchLasera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuchLasera : MonoBehaviour {
+
+    public float laserSpeed;
+    private Rigidbody rb;
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * laserSpeed;
+	}
+	
+}
