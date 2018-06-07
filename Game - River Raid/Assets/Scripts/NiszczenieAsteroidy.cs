7a14098aef6876ed552cc9e0172819cbabd147a6
Arek Szwarc <arek078@gmail.com>
2018-06-07 09:00:02 +0200

    Dodanie eksplozji

--- Game - River Raid/Assets/Scripts/NiszczenieAsteroidy.cs
+++ Game - River Raid/Assets/Scripts/NiszczenieAsteroidy.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class NiszczenieAsteroidy : MonoBehaviour {
 
-	void OnTriggerEnter(Collider other) 
+    public GameObject eksplozja;
+    public GameObject graczEksplozja;
+
+    void OnTriggerEnter(Collider other) 
     {
         if (other.tag == "Boundary")
         {
             return;
+        }
+        Instantiate(eksplozja, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(graczEksplozja, other.transform.position, other.transform.rotation);
         }
         Destroy(other.gameObject);
         Destroy(gameObject);
